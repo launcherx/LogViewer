bbf89520f6f64584840f9e0ae28e9912b0e066ba
Imants Cernovs <imantscernovs@live.com>
2023-02-23 16:59:47 +0200

    Fixed regex catastrophic backtracking bug

--- src/Entities/LogEntry.php
+++ src/Entities/LogEntry.php
@@ -309,7 +309,7 @@ class LogEntry implements Arrayable, Jsonable, JsonSerializable
         }
 
         // EXTRACT CONTEXT (Regex from https://stackoverflow.com/a/21995025)
-        preg_match_all('/{(?:[^{}]|(?R))*}/x', $header, $out);
+        preg_match_all('/{(?:[^{}]*|(?R))*}/x', $header, $out);
         if (isset($out[0][0]) && ! is_null($context = json_decode($out[0][0], true))) {
             $header = str_replace($out[0][0], '', $header);
             $this->setContext($context);
